73c843dfef5d7f5826bd02979184ea944c274d24
Erostin Lushka <elushka@bu.edu>
2020-08-17 22:07:44 -0400

    products finished

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,13 +9,54 @@ function Home() {
                 className="home__image" 
                 src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" 
                 alt="" />
-            <Product
-                id="123456"
-                title="The Lean Startup"
-                price={19.32}
-                rating={5}
-                image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
-            />
+            <div className="home__row">
+                <Product
+                    id="123456"
+                    title="Midnight Sun"
+                    price={19.32}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
+                />
+                <Product
+                    id="123456"
+                    title="Midnight Sun"
+                    price={19.32}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id="123456"
+                    title="Midnight Sun"
+                    price={19.32}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
+                />
+                <Product
+                    id="123456"
+                    title="Midnight Sun"
+                    price={19.32}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
+                />
+                <Product
+                    id="123456"
+                    title="Midnight Sun"
+                    price={19.32}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
+                />
+            </div>
+            <div className="home__row">
+                <Product
+                    id="123456"
+                    title="Midnight Sun"
+                    price={19.32}
+                    rating={5}
+                    image="https://images-na.ssl-images-amazon.com/images/I/41dd3tjpdUL._SX329_BO1,204,203,200_.jpg"
+                />
+            </div>
         </div>
     )
 }
--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -4,19 +4,25 @@ import './Product.css';
 function Product({ id, title, image, price, rating }) {
     return (
         <div className="product">
-            <p>{title}</p>
-            <p className="product__price">
-                <small>$</small>
-                <strong>{price}</strong>
-            </p>
-            <div className="product_rating">
-                {
-                    Array(rating)
-                    .fill()
-                    .map((_) => (
-                        <p>⭐</p>
-                    ))}
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {
+                        Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p>⭐</p>
+                            )
+                        )
+                    }
+                </div>
             </div>
+            <img src={image} alt="" />
+            <button>Add to Basket</button>
         </div>
     )
 }
