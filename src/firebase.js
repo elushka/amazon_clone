ec3cc026e348349b8966f6cc269c23ee6753fe2e
Erostin Lushka <elushka@users.noreply.github.com>
2020-08-19 20:54:42 -0400

    Update

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAyYsAPF8XJs4bBxSpdy9a5eL2rKXvUXpk",
+    apiKey: "",
     authDomain: "el--clone.firebaseapp.com",
     databaseURL: "https://el--clone.firebaseio.com",
     projectId: "el--clone",
     storageBucket: "el--clone.appspot.com",
-    messagingSenderId: "762771091576",
-    appId: "1:762771091576:web:9b27532a3e6e09ccc7bd8d",
-    measurementId: "G-NE7RE5CF93"
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 })
 
 const auth = firebaseApp.auth();
